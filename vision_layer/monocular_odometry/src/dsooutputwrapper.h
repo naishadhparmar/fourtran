9e7c78871c6d3be87594434d1a63920f2e0ed5c0
Naishadh Parmar <naishadh_parmar@live.com>
2018-03-19 23:54:00 +0530

    Created barebones

---
+++ vision_layer/monocular_odometry/src/dsooutputwrapper.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "boost/thread.hpp"
+#include "util/MinimalImage.h"
+#include "IOWrapper/Output3DWrapper.h"
+
+#include "FullSystem/HessianBlocks.h"
+#include "util/FrameShell.h"
+
+namespace dso {
+
+  class FrameHessian;
+  class CalibHessian;
+  class FrameShell;
+
+  namespace IOWrap {
+
+    class DSOOutputWrapper : public Output3DWrapper {
+    public:
+      inline SampleOutputWrapper() {
+          printf("Hello DSO\n");
+      }
+
+      virtual ~SampleOutputWrapper() {
+          printf("Goodbye DSO\n");
+      }
+
+      virtual void publishCamPose(FrameShell* frame, CalibHessian* HCalib) override {
+        printf("Current Frame %d (time %f, internal ID %d). CameraToWorld:\n",
+               frame->incoming_id,
+               frame->timestamp,
+               frame->id);
+        std::cout << frame->camToWorld.matrix3x4() << "\n";
+      }
+
+      virtual void pushDepthImageFloat(MinimalImageF* image, FrameHessian* KF ) override
+      {
+        printf("Predicted depth for KF %d (id %d, time %f, internal frame-ID %d). CameraToWorld:\n",
+               KF->frameID,
+               KF->shell->incoming_id,
+               KF->shell->timestamp,
+               KF->shell->id);
+        std::cout << KF->shell->camToWorld.matrix3x4() << "\n";
+
+        int maxWrite = 5;
+        for(int y=0;y<image->h;y++)
+        {
+          for(int x=0;x<image->w;x++)
+          {
+            if(image->at(x,y) <= 0) continue;
+
+            printf("Example Idepth at pixel (%d,%d): %f.\n", x,y,image->at(x,y));
+            maxWrite--;
+            if(maxWrite==0) break;
+          }
+          if(maxWrite==0) break;
+        }
+      }
+
+      virtual bool needPushDepthImage() override {
+        return false;
+      }
+
+      virtual void publishGraph(const std::map<uint64_t, Eigen::Vector2i, std::less<uint64_t>, Eigen::aligned_allocator<std::pair<const uint64_t, Eigen::Vector2i>>> &connectivity) override {}
+
+      virtual void publishKeyframes( std::vector<FrameHessian*> &frames, bool final, CalibHessian* HCalib) override {}
+
+      virtual void pushLiveFrame(FrameHessian* image) override {}
+
+      virtual void pushDepthImage(MinimalImageB3* image) override {}
+    };
+  }
+}
--- vision_layer/monocular_odometry/src/odometry.cpp
+++ vision_layer/monocular_odometry/src/odometry.cpp
@@ -1,3 +1,57 @@
+#include "dsooutputwrapper.h"
+#include "FullSystem/FullSystem.h"
+#include "util/settings.h"
+#include "util/Undistort.h"
+#include "ros/ros.h"
+#include "sensor_msgs/Image.h"
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include <cv_bridge/cv_bridge.h>
+
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <geometry_msgs/PointStamped.h>
+
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+FullSystem* fullSystem = 0;
+Undistort* undistorter = 0;
+
+string calib = "";
+string vignetteFile = "";
+string gammaFile = "";
+
+void imageCallback(const sensor_msgs::Image::ConstPtr& msg)
+{
+  cv_bridge::CvImagePtr cv_img_ptr;
+  try
+  {
+    cv_img_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    MinimalImageB minImg((int)cv_img_ptr->image.cols, (int)cv_img_ptr->image.rows,(unsigned char*)cv_img_ptr->image.data);
+    	ImageAndExposure* undistImg = undistorter->undistort<unsigned char>(&minImg, 1,0, 1.0f);
+    	fullSystem->addActiveFrame(undistImg, frameID);
+  }
+  catch(cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+}
+
 int main(int argc, char **argv)
 {
+  ros::init(argc, argc, "monocular_odometry");
+  ros::NodeHandle nh;
+
+  undistorter = Undistort::getUndistorterForFile(calib, gammaFile, vignetteFile);
+  fullSystem = new FullSystem();
+  fullSystem->linearizeOperation=false;
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber image_raw_sub = it.subscribe("/hardware_camera/camera/image_raw", 1, imageCallback);
+  ros::spin()
 }
